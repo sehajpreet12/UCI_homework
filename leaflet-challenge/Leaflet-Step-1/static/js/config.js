945e50f50a5fe4f34cce1541fb0ac914cd18313f
sehajpreet12 <71570910+sehajpreet12@users.noreply.github.com>
2021-03-02 00:41:17 -0800

    final commit

---
+++ leaflet-challenge/Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,16 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoic2VoYWpwcmVldDEyIiwiYSI6ImNra2Qwem1zZTA1eWYyb281bjFnbnpwOGIifQ.lN8X51G2lfViMqyhUntHlw";
+
+const attribution = `
+  Map data & copy;
+  <a href="https://www.openstreetmap.org/">
+    OpenStreetMap
+    </a>
+  contributors,
+    <a href="https://creativecommons.org/licenses/by-sa/2.0/">
+      CC-BY-SA
+    </a>
+    , Imagery ©
+  <a href="https://www.mapbox.com/">
+    Mapbox
+    </a>`;
